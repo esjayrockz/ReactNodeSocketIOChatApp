ffa4439cbf2d6cc884d6654ac7b130184298ef81
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-09-27 01:59:26 -0400

    Adding emitting and listening to events

--- server/server.js
+++ server/server.js
@@ -1,15 +1,36 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+let users = 0;
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log('Server is up!');
+io.on('connection', (socket) => {
+  users = users + 1;
+  console.log('New user connected, total no. of connected users:', users);
+
+  socket.on('disconnect', () => {
+    users = users - 1;
+    console.log('Client disconnected, total no. of connected users:', users);
+  });
+
+  socket.on('createMessage', (newMsg) => {
+    io.emit('newMessage', newMsg)
+  });
+
+  io.emit('newConnection', {users});
+});
+
+server.listen(port, () => {
+  console.log(`Server is up running on port ${port}`);
 });
--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import io from 'socket.io-client';
 
-const DashboardPage = () => (
-  <div>
-    <h1>Welcome to the Chat App</h1>
-  </div>
-);
+export default class DashboardPage extends React.Component {
 
-export default DashboardPage;
+  state = {
+    connected: false,
+    users: 0
+  };
+
+  componentDidMount(){
+    const socket = io();
+    socket.on('connect', () => {
+      this.setState(() => ({connected: true}));
+      console.log('Connected to server');
+      socket.emit('createMessage', {
+        text: `User ${this.state.users} is connected`
+      });
+    });
+
+    socket.on('disconnect', () => {
+      this.setState(() => ({connected: false}));
+      console.log('Disconnected to server');
+    });
+
+    socket.on('newConnection', (count) => {
+      this.setState(() => ({users: count.users}));
+    });
+
+    socket.on('newMessage', (newMsg) => {
+      console.log(newMsg.text);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Chat App</h1>
+        <p>We are {this.state.connected? 'connected':'disconnected'}</p>
+        <p>Total number of connected users: {this.state.users}</p>
+      </div>
+      );
+    }
+  }
