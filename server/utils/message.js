1caa8038e0a65b12215a082467d2b4624845a89e
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-10-01 20:18:56 -0400

    Formatting time with moment.js

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,13 +1,15 @@
+const moment = require('moment');
+
 const generateMessage = (from, text) => ({
   from,
   text,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 
 const generateLocationMessage = (from, latitude, longitude) => ({
   from,
   url: `https://www.google.com/maps/?q=${latitude},${longitude}`,
-  createdAt: new Date().getTime()
+  createdAt: moment().valueOf()
 });
 
 module.exports = {generateMessage, generateLocationMessage};
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import moment from 'moment';
 
 export default class Message extends React.Component{
 
   render(){
+
+    const formattedTime = moment(this.props.messageText.createdAt).format('h:mm a');
+
     return (
         <div>
-          <p>{this.props.messageText.from}: {this.props.messageText.text?this.props.messageText.text: <a href={this.props.messageText.url} target="_blank">My current location</a>}</p>
+          <p>{this.props.messageText.from} {formattedTime} : {this.props.messageText.text?this.props.messageText.text: <a href={this.props.messageText.url} target="_blank">My current location</a>}</p>
         </div>
     );
   }
